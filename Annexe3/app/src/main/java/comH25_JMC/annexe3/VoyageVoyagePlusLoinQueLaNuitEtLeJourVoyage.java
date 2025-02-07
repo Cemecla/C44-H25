f6399c252a6febce6185756a0324babc8d4b4435
Ecole <jcaron00@hotmail.com>
2025-02-07 10:40:16 -0500

    Annexe3

---
+++ Annexe3/app/src/main/java/comH25_JMC/annexe3/VoyageVoyagePlusLoinQueLaNuitEtLeJourVoyage.java
@@ -0,0 +1,24 @@
+package comH25_JMC.annexe3;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VoyageVoyagePlusLoinQueLaNuitEtLeJourVoyage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_voyage_voyage_plus_loin_que_la_nuit_et_le_jour_voyage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
