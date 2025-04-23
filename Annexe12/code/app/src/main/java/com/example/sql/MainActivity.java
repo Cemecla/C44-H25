69783b1dbec026828c3c62c1798c68ab81efaa3c
Ecole <jcaron00@hotmail.com>
2025-04-23 14:25:37 -0400

    annexe12b

--- Annexe12/code/app/src/main/java/com/example/sql/MainActivity.java
+++ Annexe12/code/app/src/main/java/com/example/sql/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sql;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView liste;
+    Gestionnaire_BD instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        liste = findViewById(R.id.choix_reponse);
+
+        instance = Gestionnaire_BD.getInstance(getApplicationContext()); // Avoir le contexte de toute l'application au lieu de la simple activité.
+        instance.ouvrirConexion();
+        Vector<String> choix = instance.getInventions();
+
+        //remplir le ListView
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,choix);
+        liste.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        instance.fermerConnexion();
     }
 }
+
+
+
+
+
+
+
+
